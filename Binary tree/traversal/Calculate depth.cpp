b929ac2f399e97fdcd7d8024dbf01f256a1e19b7
jason <laso85143@gmail.com>
2021-11-29 19:07:05 +0800

    1129

---
+++ Binary tree/traversal/Calculate depth.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include <vector>
+#include <list>
+#include <queue>
+#include <algorithm>
+#include <string>
+
+
+
+using namespace std;
+
+
+
+int findDepthRec (char tree[] , int n  , int & index){
+
+	if (index >= n || tree[index]=='l')
+	{
+		return 0 ;
+	}
+	index ++;
+	int left = findDepthRec(tree  , n , index);
+	index++ ;
+	int right = findDepthRec(tree , n , index);
+	return max(left , right)+1;
+}
+
+int findDepth (char tree[] , int n ){
+
+	int index = 0 ;
+	 return  findDepthRec(tree, n, index);
+
+}
+
+int main()
+{
+
+
+	 char tree[] = "nlnnlll";
+    int n = strlen(tree);
+ 
+    cout << findDepth(tree, n) << endl;
+
+
+	system("pause");
+    return 0;                           
+}
