376dbb93ef65030b356ea5475d8d7ad22c847315
jason <laso85143@gmail.com>
2022-02-18 18:17:31 +0800

    0000

---
+++ Algorithms/Sort Alogrithms/BubbleSort.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <stack>
+#include<vector>
+#include <math.h>
+#include <list>
+#include <algorithm>
+using	namespace std;
+
+
+
+
+void pswap(int *a, int *b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void printArray(int arr[], int size) {
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+
+
+void BubbleSort(int arr[], int n) {
+	int i, j;
+
+	for (i= 0;  i< n-1; i++){
+
+		for ( j = 0; j < n-i-1; j++)
+		{
+			if (arr[j] > arr[j+1])
+			{
+				pswap(&arr[j], &arr[j + 1]);
+			}
+		}
+	}
+
+
+}
+
+
+
+int main()
+{
+	int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	BubbleSort(arr, n);
+	cout << "Sorted array: \n";
+	printArray(arr, n);
+
+	system("pause");
+	return 0;
+
+}
+
+
