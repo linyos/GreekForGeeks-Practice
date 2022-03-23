844130f1847c2bdb24da5ad981ceb00a0044d035
jason <laso85143@gmail.com>
2022-03-23 18:39:03 +0800

    0323

---
+++ Graph/11. CountANumber.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include<cassert>
+
+using namespace std;
+
+class Graph
+{
+private:
+	 int  V;
+	list<int> * adj;
+public:
+	Graph(int V);
+	void addEdge(int v ,int w);
+	int BFS(int s ,  int l);
+};
+
+
+Graph::Graph(int V)
+{
+	this->V = V;
+	adj = new list<int>[V];
+}
+
+
+void Graph:: addEdge(int v , int w)
+{
+	// Add w to v¡¦s list.
+	adj[v].push_back(w);
+	// Add v to w's list.
+	adj[w].push_back(v);
+}
+
+
+int Graph::BFS(int s  ,int l)
+{
+	bool * visited = new bool[V];
+	int * level= new int [V];
+
+	for (int i = 0; i < V; i++)
+	{
+		visited[i] =false;
+		level[i] = 0;
+	}
+	  // Create a queue for BFS
+	list<int> queue;
+	// Mark the current node as
+    // visited and enqueue it
+	visited[s] =true;
+	queue.push_back(s);
+	level[s] = 0 ;
+
+	while (!queue.empty())
+	{
+		s =queue.front();
+		queue.pop_front();
+
+		for (auto i =adj[s].begin(); i != adj[s].end(); ++i)
+		{
+			if (!visited[*i])
+			{
+				level[*i] = level[s]+1;
+				visited[*i] = true;
+				queue.push_back(*i);
+			}
+		}
+	}
+
+	int count =0;
+	for (int i = 0; i < V; i++)
+	{
+		if (level[i]==l)
+		{
+			count++;
+		}
+	}
+	return count;
+
+}
+
+int  main() {
+	 Graph g(6);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+    g.addEdge(2, 5);
+ 
+    int level = 2;
+ 
+    cout << g.BFS(0, level);
+ 
+	system("pause");
+	return 0;
+}
+
+
+
---
+++ Graph/12. Count_all_PossiblePath.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include<cassert>
+
+using namespace std;
+
+class Graph
+{
+public:
+	Graph(int v);
+	void add_edge(int src , int dst);
+	int count_paths(int src , int dst ,int  v);
+
+private:
+	int m_vertices;
+	list<int> * m_neig;
+	void path_counter (int src , int dst , int & path_count , vector<bool> & visited);
+};
+
+
+Graph::Graph(int v)
+{
+	m_vertices = v;
+	m_neig = new list<int>[v];
+}
+
+
+void Graph::add_edge(int src , int dst)
+{
+	m_neig[src].push_back(dst);
+}
+
+
+int Graph::count_paths(int src , int dst , int v)
+{
+	int path_count= 0;
+	vector<bool> visited (v ,false);
+	path_counter(src , dst  , path_count , visited);
+	return path_count;
+}
+
+
+
+
+void Graph::path_counter(int src , int dst , int & path_count , vector<bool> & visited)
+{
+	visited[src] = true;
+	if (src==dst)
+	{
+		path_count++;
+	}
+	else
+	{
+		for (auto i = m_neig[src].begin() ; i!= m_neig[src].end() ; i ++ )
+		{
+			if (!visited[*i])
+			{
+				path_counter( *i ,dst , path_count , visited);
+			}
+		}
+	}
+	visited[src]= false;
+}
+
+
+//
+//void Graph::path_counter(int src , int dst , int & path_count , vector<bool> & visited)
+//{
+//	visited[src]=true;
+//	if (src==dst)
+//	{
+//		path_count ++ ;
+//	}
+//	else
+//	{
+//		for (auto neighbor : m_neig[src])
+//		{
+//			if (!visited[neighbor])
+//			{
+//				path_counter(neighbor , dst , path_count,visited);
+//			}
+//		}
+//	}
+//	visited[src] =false;
+//}
+//
+
+
+
+
+int  main() {
+	 Graph g(5);
+    g.add_edge(0, 1);
+    g.add_edge(0, 2);
+    g.add_edge(0, 4);
+    g.add_edge(1, 3);
+    g.add_edge(1, 4);
+    g.add_edge(2, 3);
+    g.add_edge(2, 1);
+    g.add_edge(3, 2);
+    // Validate it...
+    cout << g.count_paths(0, 4, 5);
+ 
+	system("pause");
+	return 0;
+}
+
+
+
