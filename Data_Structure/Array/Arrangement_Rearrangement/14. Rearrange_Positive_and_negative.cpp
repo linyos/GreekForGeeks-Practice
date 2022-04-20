be011b9477a226e7dc513475b0642c6d9fe8b777
linyos <laso85143@gmail.com>
2022-04-21 00:54:53 +0800

    0421

--- Data_Structure/Array/Arrangement_Rearrangement/14. Rearrange_Positive_and_negative.cpp
+++ Data_Structure/Array/Arrangement_Rearrangement/14. Rearrange_Positive_and_negative.cpp
@@ -24,6 +24,28 @@ void printArray(int * arr, int n)
 	printf("\n");
 }
 
+// ©µ¦ùÃD
+
+//[1,2,3,4,5,6] -> [1,3,2,4,6]
+void RearrangePosNeg2(int arr[], int n)
+{
+	int key, j;
+	for (int i = 1; i < n; i++)
+	{
+
+		key = arr[i];
+		if (key % 2 == 0) continue;
+		j = i - 1;
+
+		while (j >= 0 && arr[j] % 2 == 0)
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+
+}
 
 
 void RearrangePosNeg(int arr[], int n)
